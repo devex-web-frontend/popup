0460eec75eba3608b13d75a7cfb02f7535ab8d2d
mankdev <tema@mankdev.name>
2015-06-08 01:10:29 +0300

    bootstrap fix and bump

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Popup from 'src/Popup/PopupFactory';
-import PopupModel from 'src/Popup/PopupModel';
-import PopupView from 'src/Popup/PopupView';
+import Popup from './src/Popup/PopupFactory';
+import PopupModel from './src/Popup/PopupModel';
+import PopupView from './src/Popup/PopupView';
 
 export default Popup;
 export {PopupModel};
